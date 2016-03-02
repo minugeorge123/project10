f80050ee7002bb2ade795583382fc2f45ac1b15d
minugeorge123 <minugeorge1992@gmail.com>
2016-03-01 21:41:49 -0800

    Newly made

---
+++ sub.c
@@ -0,0 +1,13 @@
+#include<conio.h>
+void main()
+{
+	int x,y,diff;
+	printf("Enter numbers"); //arguement added
+	scanf("%d%d",&x,&y);
+	diff=x-y + y;//arduments added
+	printf("Difference: %d",diff);
+	printf("AA");
+
+        this is the new one
+
+}
